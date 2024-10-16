7a15441e54003575dcd8e35d4a1af82e9ebf1f31
eunji kang <miki2323@naver.com>
2024-10-16 14:58:28 +0900

    feat: make input func
    function skeleton 작성 및 Data Input 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,42 @@
+/*
+덧셈할 문자열을 입력해 주세요.
+1,2:3
+결과 : 6
+*/
+
+import * as readline from 'node:readline/promises'
+import { stdin as input, stdout as output } from 'node:process'
+
+// Get Input Data
+async function getString() {
+  const rl = readline.createInterface({ input, output })
+  const answer = await rl.question('덧셈할 문자열을 입력해 주세요.\n')
+  console.log(`결과 : ${answer}`)
+  rl.close();
+}
+
+// 구분자 처리 함수
+function getNumberSplit(inputString) {
+  
+}
+
+
+// Big integer Calculator
+function calculator(left, right) {
+  
+}
+
+
+// Error Handling
+function errorHandling() {
+
+}
+
 class App {
-  async run() {}
+  async run() {
+    getString();
+  }
+
 }
 
 export default App;
