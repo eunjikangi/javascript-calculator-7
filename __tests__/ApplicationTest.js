d2c9894927b27cfc34876fabc5b6165506d380ca
eunji kang <miki2323@naver.com>
2024-10-18 14:18:04 +0900

    test: testcase 추가
    10 pass / 10 total

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -32,7 +32,7 @@ describe("문자열 계산기", () => {
     });
   });
 
-  test("예외 테스트", async () => {
+  test("예외 테스트 - 음수입력", async () => {
     const inputs = ["-1,2,3"];
     mockQuestions(inputs);
 
@@ -40,4 +40,107 @@ describe("문자열 계산기", () => {
 
     await expect(app.run()).rejects.toThrow("[ERROR]");
   });
+
+  test("쉼표, 세미콜론 구분자 테스트", async () => {
+    const inputs = ["1,2:3"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 6"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("Big Integer 테스트", async () => {
+    const inputs = ["9999999999999999999999999999999999999999999999999999999999999999999999,1"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 10000000000000000000000000000000000000000000000000000000000000000000000"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("공백 입력 테스트", async () => {
+    const inputs = [""];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 0"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("공백 입력 테스트 - 헷갈림", async () => {
+    const inputs = [","];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 0"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("연속된 구분자 테스트 - 헷갈림", async () => {
+    const inputs = ["1,,,2"];
+    mockQuestions(inputs);
+
+    const logSpy = getLogSpy();
+    const outputs = ["결과 : 3"];
+
+    const app = new App();
+    await app.run();
+
+    outputs.forEach((output) => {
+      expect(logSpy).toHaveBeenCalledWith(expect.stringContaining(output));
+    });
+  });
+
+  test("커스텀 구분자  - 중복 테스트", async () => {
+    const inputs = ["//;\n1;2;3//;\n1;2;3"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("커스텀 구분자 - 숫자 입력 테스트", async () => {
+    const inputs = ["//1\n1;2;3"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+
+  test("커스텀 구분자 - 문자 중간 위치", async () => {
+    const inputs = ["123//;\n1;2;3"];
+    mockQuestions(inputs);
+
+    const app = new App();
+
+    await expect(app.run()).rejects.toThrow("[ERROR]");
+  });
+  
 });
